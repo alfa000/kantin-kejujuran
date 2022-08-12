05b44a1ed1a7a1e0d9478eb68a26f8284eb80c58
Adnan Al-farizi Rahmat <42133636+alfa000@users.noreply.github.com>
2022-08-12 16:37:38 +0700

    tambah nim

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
     </section>
 
     <footer>
-        &copy; Adnan Al-farizi Rahmat
+        &copy; 10120740 -  Adnan Al-farizi Rahmat
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
